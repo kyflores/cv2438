a6f5790cd4f63f48e9a54c8dd21ba9dce93eddd0
Kyle Flores <6565728+kyflores@users.noreply.github.com>
2022-11-20 12:10:39 -1000

    Split code in multidet up, add v4l2 configurator

--- multidet/main.py
+++ multidet/main.py
@@ -5,9 +5,12 @@ import torch
 import torchvision.ops as ops
 import numpy as np
 import cv2
-import detectors as dets
 
 import submodules.sort.sort as sort
+
+import detectors as dets
+import draw
+import camera
 
 def map_to_sort_id(dets, trackers):
     id_num, _ = trackers.shape
@@ -28,64 +31,28 @@ def map_to_sort_id(dets, trackers):
     for ix, m in enumerate(mins):
         dets[ix]['sort_id'] = trackers[m][-1]
 
-
-def draw(img, detections):
-    # TODO placeholder skip if empty
-    if len(detections) == 0:
-        return img
-
-    for d in detections:
-        corners = [(d['corners'].astype(np.int32).reshape(4, 1, 2))]
-
-        # Kind of misusing this, but I want boxes to all have different colors
-        withbox = cv2.polylines(
-            img,
-            corners,
-            isClosed=True,
-            color = d['color'],
-            thickness = 3)
-
-        newid = d.get('sort_id', -1)
-        cv2.putText(withbox,
-            'ID: {}'.format(newid),
-            d['corners'][0].astype(np.uint32),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255,255,255),
-            1)
-
-    return withbox
-
 def detect(opt):
-    # I recommend that you set the camera to the highest frame rate that yields
-    # an acceptable exposure.
-    #
-    # For instance...
-    # v4l2-ctl -d 3 -c exposure_time_absolute=166
-    #
-    # Use v4l2-ctl --all -d <devicenum> to find the appropriate control
-    # There's probably also an auto exposure mode you need to disable.
-
-    wc = opt.webcam
-
-    cap = cv2.VideoCapture(wc)
+    # See https://www.kurokesu.com/main/2020/05/22/uvc-camera-exposure-timing-in-opencv/
+    camera.config_gain_exposure(opt.webcam, opt.gain, opt.exposure)
+    cap = cv2.VideoCapture(opt.webcam)
     if (not cap.isOpened()):
         print("Could not open camera. Try change the device ID.")
         exit(1)
 
+    # These seem to kind of work, GAIN doesn't appear to apply?
+    # TODO wrap v4l2-ctl b/c that's more reliable
+
     apriltags = dets.AprilTagDetector(
         dets.C310_PARAMS,
-        'tag36h11',
-        0.168
+        opt.tag_family,
+        opt.tag_size
     )
-    yolov5 = dets.YoloV5TorchDetector('yolov5s.pt')
+    yolov5 = dets.YoloV5TorchDetector(opt.weights)
 
-    max_age = 60
-    min_hits = 3
-    iou_thresh = 0.15
-    tracker = sort.Sort(max_age=max_age,
+    min_hits = 1
+    tracker = sort.Sort(max_age=opt.max_age,
                    min_hits=min_hits,
-                   iou_threshold=iou_thresh)
+                   iou_threshold=opt.iou_thresh)
 
     while True:
         t_begin = time.time()
@@ -110,10 +77,10 @@ def detect(opt):
         # Go back through the array and assign SORT IDs to the boxes
         # based on IOU with SORT boxes.
         if trackers.shape[0] > 0:
-            newid = map_to_sort_id(all_dets, trackers)
-            # det['sort_id'] = newid
+            map_to_sort_id(all_dets, trackers) # Mutates the dictionary
 
-        with_boxes = draw(frame, all_dets)
+        with_boxes = draw.draw(frame, all_dets)
+        # with_boxes = draw.draw_sort(frame, trackers)
         cv2.imshow('detector', with_boxes)
 
         t_end = time.time()
@@ -126,7 +93,23 @@ def detect(opt):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--webcam', type=int, default=0, help='/dev/videoX number')
-    parser.add_argument('--tag-family', type=str, default='tag36h11', help='Apriltag family')
+    parser.add_argument('--gain', type=int, default=150, help='Gain to configure with v4l2-ctl')
+    parser.add_argument('--exposure', type=int, default=500, help='Exposure time to configure with v4l2-ctl')
+    parser.add_argument('--tag_family', type=str, default='tag36h11', help='Apriltag family')
+    parser.add_argument('--tag_size', type=float, default=1.0, help='Apriltag size in meters')
+    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='Path to YOLO weights file')
+    parser.add_argument(
+        '--max_age',
+        type=int,
+        default=30,
+        help='Longest time (in frames) SORT will remember an ID without a detection')
+    parser.add_argument(
+        '--iou_thresh',
+        type=float,
+        default='0.15',
+        help='IOU threshold for SORT. Smaller can track faster movements but reduces accuracy')
+
+
     opt = parser.parse_args()
 
     detect(opt)
--- multidet/readme.md
+++ multidet/readme.md
@@ -15,7 +15,7 @@ for each detection type
 * `color` RGB triple for the box color.
 * `corners` of the bounding area of the detection
   * numpy array, 4 rows, 2 columns, shape = (4, 2)
-* `sort_xyxy`, xmin, xmax, ymin, ymax format for a bouding box.
+* `sort_xyxy`, xmin, xmax, ymin, ymax format for a bounding box.
   * This format is required by the SORT tracker, and is mostly
     redunant with `corners`
 
@@ -26,3 +26,36 @@ exist, but are not guaranteed for every object.
 * `distance` (stereo depth estimator, apriltags)
 * `pose` (apriltags)
 * `confidence` (yolov5)
+* `sort_id` (SORT)
+
+## Camera configuration
+Most USB webcams have a few configurable parameters. To minimize
+motion blur we want to operate at the fastest exposure time possible,
+even if that doesn't always translate to more frames delivered per second.
+
+In this context is a device number corresponding to /dev/videoX.
+Here's an example command set for configuring the Logitech C310.
+Pass `-C` instead of `-c` to read a property instead of set it.
+```
+v4l2-ctl --list-devices # Find your /dev/videoX number
+v4l2-ctl --all -d device # Show all configuration properties
+v4l2-ctl -d 3 -c auto_exposure=1
+v4l2-ctl -d 3 -c gain=200
+v4l2-ctl -d 3 -c exposure_time_absolute=250
+```
+
+OpenCV can configure these parameters with the `cv2.CAP_PROP_*` fields,
+but just from experimenting with it, it doesn't seem reliable. Setting
+the gain for instance changes the number reported by `v4l2-ctl` but
+doesn't affect the image.
+
+## TODOs
+* Improve mapping SORT boxes back to detect boxes. Current method allows
+double assignment, and this seems bugged
+* Add blob detector module. Kind of questionable b/c of how much scene
+specific tuning and filtering it needs.
+* Integrate `robotpy` for network tables support
+* Setup the camera framerate automatically, maybe by v4l2-ctl call
+* Add Yolo OpenVINO backend support for inference on Intel targets
+* Support streaming the augmented camera feed to the driver station
+* Add stereo depth map if second camera is available
